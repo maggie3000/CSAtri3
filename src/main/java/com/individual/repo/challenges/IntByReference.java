aff6e377a70aa719e2a5b835e4a0e5d0c3132dcd
maggie3000 <maggiekillada@gmail.com>
2022-03-11 10:00:42 -0800

    intial commit

---
+++ src/main/java/com/individual/repo/challenges/IntByReference.java
@@ -0,0 +1,65 @@
+package com.individual.repo.challenges;
+
+/*
+Write a Java Class with Method(s) to swap two numbers and add to Menu.
+Key learning 1: Pass-by-Value and Pass-by-Reference are two key concepts when passing
+parameters through code, to do a Swap you will need to consider changing value in the referenced objects.
+Key learning 2: Swap is a fundamental concept when sorting data.
+Key learning 3: start working with toString method, this method is used to represent data in
+Class using a formatted String. Here is some starter code on creating a
+Class to swap values by reference by creating you own data type...
+ */
+
+/*
+public class IntByReference {
+    private int value;
+
+    // Hack: create IntByReference, swapToLowHighOrder and toString methods
+
+    // constructor for initializing number value variable
+    public IntByReference(Integer value){
+        this.value = value;
+    }
+
+    public String swapToLowHighOrder(int number){
+        if (this.value < number){
+            int temp = value;
+            // this.value
+        }
+    }
+
+
+
+    // static method that enables me to see numbers swapped by reference (before, after)
+
+    public static void swapper(int n0, int n1) {
+        IntByReference a = new IntByReference(n0);
+        IntByReference b = new IntByReference(n1);
+        System.out.println("Before: " + a + " " + b);
+        a.swapToLowHighOrder(b);  // conditionally build swap method to change values of a, b
+        System.out.println("After: " + a + " " + b);
+        System.out.println();
+    }
+
+    // static main method that provides some simple test cases
+    public static void main(String[] ags) {
+        IntByReference.swapper(21, 16);
+        IntByReference.swapper(16, 21);
+        IntByReference.swapper(16, -1);
+    }
+}
+*/
+
+/*
+SAMPLE OUTPUT
+=============
+
+Before: 21 16
+After: 16 21
+
+Before: 16 21
+After: 16 21
+
+Before: 16 -1
+After: -1 16
+ */
